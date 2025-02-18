26f48ce3d57c04a8210755d43eed225cabb5c673
gvspavankumar1996 <gvspavankumar1996@gmail.com>
2025-02-18 22:59:57 +0530

    changes

--- machine coding/react/my-react-app/src/App.jsx
+++ machine coding/react/my-react-app/src/App.jsx
@@ -2,15 +2,15 @@ import "./App.css";
 import StarComponent from "./components/startComponent";
 import ToastComponent from "./components/reactToast";
 import Pagination from "./components/pagination";
-import ReactTabs from "./components/tabForm";
+import ReactTabsForm from "./components/tabForm";
 
 function App() {
   return (
     <div >
       {/* <StarComponent starCount={10} /> */}
       {/* <ToastComponent starCount={10} /> */}
-      {/* <Pagination /> */}
-      <ReactTabs/>
+      {/* <ReactTabsForm/> */}
+      <Pagination />
     </div>
   );
 }
--- machine coding/react/my-react-app/src/components/pagination/index.jsx
+++ machine coding/react/my-react-app/src/components/pagination/index.jsx
@@ -1,60 +1,43 @@
 import React, { useEffect, useState } from "react";
 import "./index.css";
 
+const ProductCard = ({ image, title }) => {
+  return (
+    <div className="product-card">
+      <div className="img-cointainer">
+        <img src={image} alt={title} className="prod-img" />
+      </div>
+
+      <span>{title}</span>
+    </div>
+  );
+};
+
 const Pagination = () => {
-  const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(2);
-  const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products?limit=100");
-    const json = await res.json();
-    setProducts(json?.products || []);
+  const [products, setProducts] = useState(null);
+
+  const fetchData = async () => {
+    const data = await fetch("https://dummyjson.com/products?limit=500");
+    const json = await data.json();
+    console.log(json);
+    setProducts(json?.products);
   };
+
   useEffect(() => {
-    fetchProducts();
+    fetchData();
   }, []);
 
-  const selectPageHandler = (selectedPage) => {
-    if (
-      selectedPage >= 1 &&
-      selectedPage <= products.length / 10 &&
-      selectedPage !== page
-    ) {
-      setPage(selectedPage);
-    }
-  };
-
   return (
-    <div>
-      {products?.length > 0 && (
-        <div className="products">
-          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+    <div className="App">
+      <h1>Pagination</h1>
+      <div className="products-container">
+        {products &&
+          products.map((p) => {
             return (
-              <span className="products__single" key={prod.id}>
-                <img src={prod.thumbnail} alt={prod.title} />
-                <span>{prod.title}</span>
-              </span>
+              <ProductCard key={p.id} image={p.thumbnail} title={p.title} />
             );
           })}
-        </div>
-      )}
-
-      {products?.length > 0 && (
-        <div className="pagination">
-          <span onClick={() => selectPageHandler(page - 1)}>⬅️</span>
-          {[...Array(products.length / 10)].map((_, i) => {
-            return (
-              <span
-                className={page === i + 1 ? "products__selected" : ""}
-                onClick={() => selectPageHandler(i + 1)}
-                key={i}
-              >
-                {i}
-              </span>
-            );
-          })}
-          <span onClick={() => selectPageHandler(page + 1)}>▶️</span>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
